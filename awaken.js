cb22c81268581e0ac8cf7f6453c1bbd5a2609920
captjay98 <captjay98@gmail.com>
2024-01-09 03:49:47 +0100

    Switched to express

--- awaken.js
+++ awaken.js
@@ -1,6 +1,8 @@
-#!/usr/bin/env node
 
-const axios = require("axios");
+
+const express = require('express');
+const axios = require('axios');
+const app = express();
 
 const sites = [
   {
@@ -27,38 +29,28 @@ const sites = [
     name: "Ticketer Koyeb",
     url: "https://ticketer-captjay.koyeb.app",
   },
- 
-];
-
-/**
- * Keeps the application awake by making HTTP requests to a list of websites.
- *
- * @param {Array} sites - An array of objects representing websites to be awakened.
- *                        Each object should have a 'name' property specifying the name of the website,
- *                        and a 'url' property specifying the URL of the website.
- * @return {Promise} A promise that resolves when all the HTTP requests have completed.
- */
+]
 
 async function keepAwake() {
-  sites.forEach(async (site) => {
+  for (const site of sites) {
     try {
       await axios.get(site.url);
       console.log(`${site.name} Awake`);
     } catch (error) {
       console.log(`Failed to wake ${site.name} with ${error.code}`);
     }
-  });
+  }
 }
 
-/**
- * Runs the specified function after ten minutes.
- *
- * @param {function} func - The function to run.
- * @return {void} This function does not return a value.
- */
-function runAfterTenMinutes() {
-  keepAwake();
-  setTimeout(keepAwake, 600000);
-}
 
-runAfterTenMinutes();
+app.get('/', async (req, res) => {
+  await keepAwake();
+  res.send('Awake requests sent to all sites.');
+});
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+  setInterval(keepAwake, 600000);
+});
